406061752d46fad296ccccfed6f3b7935adfa906
JoaoPdSsilva <joaopedro123jp1@outlook.com>
2023-01-27 21:57:22 -0300

    Começo do Teste

---
+++ view/login.php
@@ -0,0 +1,38 @@
+<?php>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>N Soluções</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../model/login.css">
+
+</head>
+<body class="text-center">
+
+<main class="form-signin w-100 m-auto">
+  <form>
+  <h1 class="h3 mb-3 fw-normal">Entrar</h1>
+    <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Usuario">
+      <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Senha">
+      <label for="floatingPassword">Senha</label>
+    </div>
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Logar</button>
+  </form>
+</main>
+
+
+    
+</body>
+</html>
